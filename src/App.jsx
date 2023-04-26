90b84cdb0d7c22d64d6d202b0caf9f9b6a95b35d
Ahmad <ahmad749nasser123@gmail.com>
2023-04-26 11:34:18 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/Footer";
+import Products from "./Components/Products";
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Products />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
